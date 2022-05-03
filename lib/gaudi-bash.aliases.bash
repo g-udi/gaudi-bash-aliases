ba4a380494a61b5ee239cfe27c505839c1a98bab
Ahmad Assaf <ahmad.a.assaf@gmail.com>
2022-05-03 09:36:39 +0100

    Renaming the gaudi-bash aliases

---
+++ lib/gaudi-bash.aliases.bash
@@ -0,0 +1,7 @@
+cite about-alias
+about-alias 'Aliases for the gaudi-bash command (these aliases are automatically included with the "general" aliases)'
+
+# Common misspellings of gaudi-bash
+alias gudi-bash='gaudi-bash'
+alias g-udi-bash='gaudi-bash'
+alias gadi-bash='gaudi-bash'
